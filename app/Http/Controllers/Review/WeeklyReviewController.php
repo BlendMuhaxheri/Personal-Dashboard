a23ef4af242ba2c54630d0bfe540ff0c9746ac56
blend <blend.muhaxheri@skruvat.se>
2026-02-10 17:49:15 +0100

    refactor

--- app/Http/Controllers/Review/WeeklyReviewController.php
+++ app/Http/Controllers/Review/WeeklyReviewController.php
@@ -3,12 +3,8 @@
 namespace App\Http\Controllers\Review;
 
 use App\Http\Controllers\Controller;
-use App\Models\Habit;
-use App\Models\Task;
 use App\Services\WeeklyReview\WeeklyReviewData;
-use Illuminate\Http\Request;
 
-use function Illuminate\Support\now;
 
 class WeeklyReviewController extends Controller
 {
